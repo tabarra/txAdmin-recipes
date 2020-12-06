0cbddf2c2cd1d610b306d713834431efcf6b36e4
Andre Tabarra <tabarra@vexguard.net>
2020-12-05 23:58:56 -0300

    plume: wip loadingscreen

---
+++ plume_esx/loadingscreen/fxmanifest.lua
@@ -0,0 +1,11 @@
+author 'PlumeESX'
+description 'Example loading screen originally based on Loqrin\'s page design.'
+version '1.0.0'
+
+files {
+    'public/index.html',
+    'public/css/styles.css',
+    'public/config.json',
+}
+
+loadscreen 'public/index.html'
---
+++ plume_esx/loadingscreen/public/config.js
@@ -0,0 +1,21 @@
+const config = {
+    serverTitle: "{{serverName}}",
+    controls: [
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+    ],
+    features: [
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+        "xxxx",
+    ],
+}
